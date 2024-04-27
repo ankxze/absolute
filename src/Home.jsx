b34b1f2989876cb7b3271452f9f7b8ba20dacd5b
ankxze <168246751+ankxze@users.noreply.github.com>
2024-04-27 04:50:16 +0300

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css'
+
+
+
+  const home = () => {
+    return (
+    <>
+    <html id="scroll">
+    <div id="hh1">
+    <body id="telo"></body>
+    <body id="telo2"></body>
+    <h1 id='text'>ABSOLUTE</h1>
+    <p1 id="text2">Лучший чат бот для вашей беседы</p1>
+    </div>
+    <div id="hh2">
+    <h1 id='text3'>ADD BOT</h1>
+    <p2 id="text4">&nbsp;Чат менеджер для модерации ваших<br></br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;чатов,&nbsp; рп действий,&nbsp; 
+      решения<br></br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;множество задач и создания<br></br> &nbsp;интерактива, &nbsp;а так же для
+       приятного<br></br> &nbsp;&nbsp;время провождения в вашей беседе. </p2>
+    <div id="d2"></div>
+    <a href="https://t.me/AbsoluteChBot" target="_blank" id="text5">Добавить бота</a>
+
+      </div>
+      </html>
+    </>
+  )
+}
+
+export default home
+
+
+
